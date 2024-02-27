637f99bcdf04dd88eb441324b9c83d8e53f3d270
RAZASHAH009 <officialraza262@gmail.com>
2024-02-27 21:27:17 +0500

    update

---
+++ app.js
@@ -0,0 +1,74 @@
+
+
+//   Chapter 1
+
+//Q1
+alert("Error! Please enter a valid password"); 
+
+//Q2
+alert("Welcome to JS land...\n Happy coding");
+
+//Q3
+alert("welcome to JS land...");
+
+//Q4
+
+
+//Q5
+alert("Hello...I can run JS through my web browser console ");
+
+
+//    Chapter 2
+   
+//Q1
+var abc = "Hello World";
+alert(abc)
+
+//Q2
+
+alert("PIZZA\nPIZZ\nPIZ\nPI\n\P")
+
+//Q3
+
+alert("My email address is example@example.com")
+
+//Q5
+
+var xyz ="A smarther way to  learn Javascript"
+alert("I am trying to learn from the book\n" + xyz)
+
+
+//Q6
+
+document.write("Yah!I can write HTML content through JavaScript <br> ")
+
+//Q7
+var efg = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(efg)
+
+//chapter 3
+
+//Q1
+var age ="15 year old"
+alert("i am" + age)
+
+
+//Q2
+
+var vist ="You Have visted site 14 time"
+alert(vist)
+
+
+
+
+//Q3
+var birth =" 5/22/2005 "
+document.write("<br> my birth day"+  birth + "<br> data type of my ddeclared variable is number")
+
+//Q4
+
+document.write("<br><br> <b>Jonh Doe</b> ordered <b>5 T Shirt</b>(s) on XYZ Clothing store  ")
+
+
+
+
